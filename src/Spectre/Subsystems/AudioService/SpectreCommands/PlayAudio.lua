e6fa18ace6a16ac0254f4d68d35968ba6a6f0939
Brandan D. <brandandelafuente@gmail.com>
2024-01-01 09:13:47 -0500

    Initial commit 2

--- src/Spectre/Subsystems/AudioService/SpectreCommands/PlayAudio.lua
+++ src/Spectre/Subsystems/AudioService/SpectreCommands/PlayAudio.lua
@@ -4,6 +4,8 @@ local Spectre = ServerScriptService["Spectre"]
 local SpectreAudio = require(script.Parent.Parent)
 local Modules = {}
 
+local MessageService = require(Spectre["Subsystems"]["MessageService"])
+
 for i,v in pairs(Spectre["Modules"]:GetChildren()) do
 	Modules[v.Name] = require(v)
 end
@@ -16,8 +18,22 @@ return {
 
 	Exec = function(plr,args)
 		local sObj = SpectreAudio:GetObject()
-		if #args > 0 then
-			local asset = MarketplaceService:GetProductInfo()
+		
+		local asset
+		
+		if #args > 0 and tonumber(args[1])~=nil then
+			asset = MarketplaceService:GetProductInfo(tonumber(args[1]),Enum.InfoType.Asset)
+			
+			sObj.SoundId = `rbxassetid://{args[1]}`
+			
+			MessageService:PushMessage({
+				MessagePresentation = "Hint",
+				MessageType = "Standard",
+				String = `Now playing {asset.Name} ({args[1]})`
+			}, plr)
+			
+			sObj:Play()
+			
 		end
 	end,
 }
