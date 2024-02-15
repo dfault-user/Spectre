b2d71c0b87f187e8853ae50f6a36c77f689ea05a
Brandan D. <brandandelafuente@gmail.com>
2024-02-14 20:43:38 -0500

    Prepare move to Libraries

--- src/Spectre/Modules/PlayerExistsInTable.lua
+++
@@ -1,9 +0,0 @@
-return function(plr: Player, tbl: {})
-	local exists = false
-
-	for i, v in pairs(tbl) do
-		if v == plr.Name then exists = true end
-	end
-
-	return exists
-end
--- src/Spectre/Settings.lua
+++ src/Spectre/Settings.lua
@@ -1,8 +1,24 @@
+export type AccessLevel = "Player" | "VIP" | "Moderator" | "Administrator" | "Owner"
+local Players = game:GetService("Players")
 Settings = {
 	Seperator = "/",
 
-	Admins = {
-		"Control22",
+	AccessLevel = {
+		Owner = {
+			`{Players:GetNameFromUserIdAsync(game.CreatorId)}.{game.CreatorId}`, -- "Control22:18875912"
+		},
+
+		Administrator = {
+
+		},
+
+		Moderator = {
+
+		},
+
+		VIP = {
+			
+		}
 	},
 
 	Banned = {},
