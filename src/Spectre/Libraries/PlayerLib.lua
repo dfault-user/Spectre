dcc2a585fbe4f37138b5af4993f5bc233a966e8c
Brandan D. <brandandelafuente@gmail.com>
2024-02-14 22:26:50 -0500

    PlayerLib implementation

--- src/Spectre/Libraries/PlayerLib.lua
+++ src/Spectre/Libraries/PlayerLib.lua
@@ -1,13 +1,36 @@
+local ServerScriptService = game:GetService("ServerScriptService")
+local Spectre = ServerScriptService["Spectre"]
+local Modules = {}
+
+for i, v in pairs(Spectre["Modules"]:GetChildren()) do
+	Modules[v.Name] = require(v)
+end
+
 local PlayerLib = {
-    SpectreSettings = require(script.Parent.Parent.Settings),
-    RequiringObject = nil,
+	RequiringObject = nil,
 }
 
-function PlayerLib:CheckAccess(Player: Player)
+function PlayerLib:CheckAccess(CurrentAccessTable: {
+	['Owner']: {}, ['Administrator']: {}, ['Moderator']: {}, ['VIP']: {}
+	}, Player: Player)
+	
+	if CurrentAccessTable then
+		for AccessDict, PlayersInDict  in pairs(CurrentAccessTable) do
+			local PlayerHasAccess = (table.find(PlayersInDict, `{Player}.{Player.UserId}`) ~= nil)
+			
+			if PlayerHasAccess then
+			return {
+				HasAccess = PlayerHasAccess, 
+				AccessLevel = `{AccessDict}`
+			}
+			else 
+				return false
+			end
+		end
+	end
 	
 end
 
-return function(Req: LuaSourceContainer)
-    PlayerLib.RequiringObject = Req
+return function(RequiringObject: LuaSourceContainer)
     return PlayerLib
 end
