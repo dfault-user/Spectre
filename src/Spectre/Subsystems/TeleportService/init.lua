8e53e50d7717ed4240f19ca9138702de230ffe9e
Brandan D. <brandandelafuente@gmail.com>
2024-02-14 21:33:34 -0500

    Stage TeleportService ShiftPlayers

--- src/Spectre/Subsystems/TeleportService/init.lua
+++ src/Spectre/Subsystems/TeleportService/init.lua
@@ -1,4 +1,15 @@
 -- TODO beta-1
 TeleportService = {}
 
+function TeleportService:ShiftPlayers(player1: Player, player2: Player)
+    local player1Character = player1.Character
+    local player2Character = player2.Character
+    local player1CFrame = player1Character["HumanoidRootPart"].CFrame
+    local player2CFrame = player2Character["HumanoidRootPart"].CFrame
+end
+
+TeleportService.API = {
+    ShiftPlayers = TeleportService.ShiftPlayers
+}
+
 return TeleportService
